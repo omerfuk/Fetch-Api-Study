66c2400506e1eb59142238e7a04c6951727ac1bd
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-25 22:10:10 +0300

    Initial Commit

---
+++ Fetch Api Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fetch Api Study
+//
+//  Created by Ömer Faruk Kılıçaslan on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
